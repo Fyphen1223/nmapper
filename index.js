bad6ba5c1e982755e3623281c555c164c41d07c7
fyphen <fyphensub@gmail.com>
2024-11-28 20:34:40 +0900

    Nmapスキャン対象をharvard.eduに変更し、出力ファイル名をres.jsonに更新。JSON変換処理を改善し、不要なコードを整理。

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import nmap from './src/index.js';
 
 async function main() {
-	const res = await nmap.scan('kansai.soka.ed.jp');
+	const res = await nmap.scan('harvard.edu');
 	fs.writeFileSync('output.txt', res);
 	console.log('Scan completed');
 	const r = await nmap.parseNmapOutput(res);
@@ -12,9 +12,9 @@ async function main() {
 async function formatResult() {
 	const output = fs.readFileSync('output.txt').toString();
 	const res = await nmap.parseNmapOutput(output);
-	fs.writeFileSync('output.json', JSON.stringify(res, null, 4));
+	fs.writeFileSync('./res.json', JSON.stringify(res, null, 4));
 }
 
-main();
-//formatResult();
+//main();
+formatResult();
 export default { nmap };
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const exec = util.promisify(child_process.exec);
 
 async function scan(ip) {
 	const res = await exec(
-		`sudo nmap ${createArguments([
+		`${process.platform == 'win32' ? 'nmap' : 'sudo nmap'} ${createArguments([
 			ip,
 			'-oX',
 			'-',
@@ -26,93 +26,137 @@ function createArguments(list) {
 	return list.join(' ');
 }
 
+/*
 function formatJSON(nmapOutput) {
-	let temp = nmapOutput.nmaprun;
-	delete temp['$'];
-	delete temp['scaninfo'];
-	delete temp['verbose'];
-	delete temp['debugging'];
-	temp['host']['time'] = temp['host']['$'];
-	delete temp['host']['$'];
-	temp['host']['status'] = temp['host']['status']['$'];
-	delete temp['host']['status']['$'];
-	temp['host']['address'] = temp['host']['address']['$'];
+	let r = nmapOutput.nmaprun;
+	delete r['$'];
+	delete r['hosthint'];
+	delete r['scaninfo'];
+	delete r['verbose'];
+	delete r['debugging'];
+	r['host']['time'] = r['host']['$'];
+	delete r['host']['$'];
+	r['host']['status'] = r['host']['status']['$'];
+	delete r['host']['status']['$'];
+	r['host']['address'] = r['host']['address']['$'];
 
 	/*
-	temp['host']['hostnames'] = temp['host']['hostnames']['hostname'];
+	r['host']['hostnames'] = r['host']['hostnames']['hostname'];
 	let hostnames = [];
-	if (typeof temp['host']?.['hostnames']?.['hostname']?.['$'] === 'object') {
-		hostnames.push(temp['host']['hostnames']['hostname']['$']);
+	if (typeof r['host']?.['hostnames']?.['hostname']?.['$'] === 'object') {
+		hostnames.push(r['host']['hostnames']['hostname']['$']);
 	} else {
-		temp['host']['hostnames'].map((hostname) => {
+		r['host']['hostnames'].map((hostname) => {
 			hostnames.push(hostname['$']);
 			delete hostname['$'];
 		});
 	}
 
-	temp['host']['hostnames'] = hostnames;
-	*/
-	temp['host']['uptime'] = temp['host']['uptime']['$'];
-	temp['host']['distance'] = temp['host']['distance']['$'];
-	temp['host']['tcpsequence'] = temp['host']['tcpsequence']['$'];
-	temp['host']['ipidsequence'] = temp['host']['ipidsequence']['$'];
-	temp['host']['tcptssequence'] = temp['host']['tcptssequence']['$'];
-	if (temp['host']['trace']?.['$']) {
-		temp['host']['trace']['used'] = temp['host']['trace']['$'];
-		delete temp['host']['trace']['$'];
+	r['host']['hostnames'] = hostnames;
+	if (r['host']?.['uptime']) {
+		r['host']['uptime'] = r['host']['uptime']['$'];
 	}
-	if (temp['host']['trace']?.['hop']?.['$']) {
-		temp['host']['trace']['hop'] = temp['host']['trace']['hop']['$'];
-		delete temp['host']['trace']['$'];
+	r['host']['distance'] = r['host']['distance']['$'];
+	r['host']['tcpsequence'] = r['host']['tcpsequence']['$'];
+	r['host']['ipidsequence'] = r['host']['ipidsequence']['$'];
+	r['host']['tcptssequence'] = r['host']['tcptssequence']['$'];
+	if (r['host']['trace']?.['$']) {
+		r['host']['trace']['used'] = r['host']['trace']['$'];
+		delete r['host']['trace']['$'];
+	}
+	if (r['host']['trace']?.['hop']?.['$']) {
+		r['host']['trace']['hop'] = r['host']['trace']['hop']['$'];
+		delete r['host']['trace']['$'];
 	}
 
-	temp['host']['ports']['extraports']['state'] =
-		temp['host']['ports']['extraports']['$'];
-	delete temp['host']['ports']['extraports']['$'];
-	temp['host']['ports']['extraports']['extrareasons'] =
-		temp['host']['ports']['extraports']['extrareasons']['$'];
-	temp['host']['ports']['port'].forEach((port) => {
-		temp['host']['ports']['port'][temp['host']['ports']['port'].indexOf(port)] =
+	r['host']['ports']['extraports']['state'] = r['host']['ports']['extraports']['$'];
+	delete r['host']['ports']['extraports']['$'];
+	r['host']['ports']['extraports']['extrareasons'] =
+		r['host']['ports']['extraports']['extrareasons']['$'];
+	r['host']['ports']['port'].forEach((port) => {
+		r['host']['ports']['port'][r['host']['ports']['port'].indexOf(port)] =
 			transformJSON(port);
 	});
-	temp['host']['os'] = transformOSJSON(temp['host']['os']);
+	r['host']['os'] = transformOSJSON(r['host']['os']);
 
-	return temp;
+	return r;
 }
+*/
 
 function transformJSON(data) {
-	function clean(obj) {
-		if (Array.isArray(obj)) {
-			return obj.map(clean);
-		} else if (typeof obj === 'object' && obj !== null) {
-			let result = {};
-			for (let key in obj) {
-				if (key === '$') {
-					Object.assign(result, obj[key]); // Expand `$` into parent
-				} else if (key === 'elem') {
-					if (Array.isArray(obj[key])) {
-						obj[key].forEach((subElem) => {
-							if (subElem._ && subElem.$ && subElem.$.key) {
-								result[subElem.$.key] = subElem._;
-							}
-						});
-					}
-				} else if (key === 'table') {
-					// Transform `table` to an array of simplified objects
-					result[key] = obj[key].map(clean);
-				} else {
-					result[key] = clean(obj[key]);
-				}
-			}
-			return result;
-		}
-		return obj; // Return primitive types as-is
-	}
+	let r = data.nmaprun;
+	r = mergeDollarKeys(r);
+	r = manipulateScript(r);
+	r = manipulateElem(r);
+	return r;
+}
 
-	return clean(data);
+function mergeDollarKeys(data) {
+	if (Array.isArray(data)) {
+		return data.map(mergeDollarKeys);
+	} else if (typeof data === 'object' && data !== null) {
+		if ('$' in data) {
+			const mergedData = { ...data, ...data['$'] };
+			delete mergedData['$'];
+			for (const key in mergedData) {
+				mergedData[key] = mergeDollarKeys(mergedData[key]);
+			}
+			return mergedData;
+		} else {
+			for (const key in data) {
+				data[key] = mergeDollarKeys(data[key]);
+			}
+			return data;
+		}
+	} else {
+		return data;
+	}
+}
+
+function manipulateScript(script) {
+	if (Array.isArray(script)) {
+		return script.map(manipulateScript);
+	} else if (typeof script === 'object' && script !== null) {
+		for (const key in script) {
+			script[key] = manipulateScript(script[key]);
+		}
+		if ('script' in script) {
+			if (!Array.isArray(script.script)) {
+				script.script = [script.script];
+			}
+		}
+		return script;
+	} else {
+		return script;
+	}
+}
+
+function manipulateElem(script) {
+	if (Array.isArray(script)) {
+		return script.map(manipulateElem);
+	} else if (typeof script === 'object' && script !== null) {
+		for (const key in script) {
+			script[key] = manipulateElem(script[key]);
+		}
+		if ('elem' in script) {
+			console.log(script.elem);
+			if (!Array.isArray(script.elem)) {
+				script.elem = [script.elem];
+			}
+			script.elem.forEach((e) => {
+				e[e.key] = e['_'];
+				delete e['_'];
+				delete e['key'];
+			});
+		}
+		return script;
+	} else {
+		return script;
+	}
 }
 
 function transformOSJSON(input) {
+	console.log(input);
 	function transform(obj) {
 		if (Array.isArray(obj)) {
 			// Handle array elements recursively
@@ -145,7 +189,7 @@ function bringUp$(d) {
 
 async function parseNmapOutput(nmapOutput) {
 	const jsonResult = await parseStringPromise(nmapOutput, { explicitArray: false });
-	return formatJSON(jsonResult);
+	return transformJSON(jsonResult);
 }
 
 export default { scan, parseNmapOutput, bringUp$ };
