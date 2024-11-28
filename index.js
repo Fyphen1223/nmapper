318493d1e3f1498ad4664246b6e1f5f3209c2779
Fyphen <fyphensub@gmail.com>
2024-11-28 07:05:12 +0000

    Nmapスキャン対象の変更と出力形式の改善、JSON変換機能の追加

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import fs from 'node:fs';
 import nmap from './src/index.js';
 
 async function main() {
-	const res = await nmap.scan('scanme.nmap.org');
+	const res = await nmap.scan('kansai.soka.ed.jp');
 	fs.writeFileSync('output.txt', res);
+	console.log('Scan completed');
+	const r = await nmap.parseNmapOutput(res);
+	fs.writeFileSync('output.json', JSON.stringify(r, null, 4));
 }
 
 async function formatResult() {
@@ -12,6 +15,6 @@ async function formatResult() {
 	fs.writeFileSync('output.json', JSON.stringify(res, null, 4));
 }
 
-//main();
-formatResult();
+main();
+//formatResult();
 export default { nmap };
--- src/index.js
+++ src/index.js
@@ -38,26 +38,114 @@ function formatJSON(nmapOutput) {
 	delete temp['host']['status']['$'];
 	temp['host']['address'] = temp['host']['address']['$'];
 
+	/*
 	temp['host']['hostnames'] = temp['host']['hostnames']['hostname'];
 	let hostnames = [];
-	temp['host']['hostnames'].map((hostname) => {
-		hostnames.push(hostname['$']);
-		delete hostname['$'];
-	});
+	if (typeof temp['host']?.['hostnames']?.['hostname']?.['$'] === 'object') {
+		hostnames.push(temp['host']['hostnames']['hostname']['$']);
+	} else {
+		temp['host']['hostnames'].map((hostname) => {
+			hostnames.push(hostname['$']);
+			delete hostname['$'];
+		});
+	}
+
 	temp['host']['hostnames'] = hostnames;
+	*/
 	temp['host']['uptime'] = temp['host']['uptime']['$'];
 	temp['host']['distance'] = temp['host']['distance']['$'];
 	temp['host']['tcpsequence'] = temp['host']['tcpsequence']['$'];
 	temp['host']['ipidsequence'] = temp['host']['ipidsequence']['$'];
 	temp['host']['tcptssequence'] = temp['host']['tcptssequence']['$'];
-	temp['host']['trace']['used'] = temp['host']['trace']['$'];
-	temp['host']['trace']['hop'] = temp['host']['trace']['hop']['$'];
-	delete temp['host']['trace']['$'];
+	if (temp['host']['trace']?.['$']) {
+		temp['host']['trace']['used'] = temp['host']['trace']['$'];
+		delete temp['host']['trace']['$'];
+	}
+	if (temp['host']['trace']?.['hop']?.['$']) {
+		temp['host']['trace']['hop'] = temp['host']['trace']['hop']['$'];
+		delete temp['host']['trace']['$'];
+	}
+
+	temp['host']['ports']['extraports']['state'] =
+		temp['host']['ports']['extraports']['$'];
+	delete temp['host']['ports']['extraports']['$'];
+	temp['host']['ports']['extraports']['extrareasons'] =
+		temp['host']['ports']['extraports']['extrareasons']['$'];
+	temp['host']['ports']['port'].forEach((port) => {
+		temp['host']['ports']['port'][temp['host']['ports']['port'].indexOf(port)] =
+			transformJSON(port);
+	});
+	temp['host']['os'] = transformOSJSON(temp['host']['os']);
+
 	return temp;
 }
+
+function transformJSON(data) {
+	function clean(obj) {
+		if (Array.isArray(obj)) {
+			return obj.map(clean);
+		} else if (typeof obj === 'object' && obj !== null) {
+			let result = {};
+			for (let key in obj) {
+				if (key === '$') {
+					Object.assign(result, obj[key]); // Expand `$` into parent
+				} else if (key === 'elem') {
+					if (Array.isArray(obj[key])) {
+						obj[key].forEach((subElem) => {
+							if (subElem._ && subElem.$ && subElem.$.key) {
+								result[subElem.$.key] = subElem._;
+							}
+						});
+					}
+				} else if (key === 'table') {
+					// Transform `table` to an array of simplified objects
+					result[key] = obj[key].map(clean);
+				} else {
+					result[key] = clean(obj[key]);
+				}
+			}
+			return result;
+		}
+		return obj; // Return primitive types as-is
+	}
+
+	return clean(data);
+}
+
+function transformOSJSON(input) {
+	function transform(obj) {
+		if (Array.isArray(obj)) {
+			// Handle array elements recursively
+			return obj.map(transform);
+		} else if (typeof obj === 'object' && obj !== null) {
+			const newObj = {};
+			for (const key in obj) {
+				if (key === '$') {
+					// Merge `$` key's value into the parent object
+					Object.assign(newObj, transform(obj[key]));
+				} else {
+					// Process nested objects
+					newObj[key] = transform(obj[key]);
+				}
+			}
+			return newObj;
+		}
+		return obj; // Return non-object types as-is
+	}
+
+	return transform(input);
+}
+function bringUp$(d) {
+	if (d['$']) {
+		Object.assign(d, d['$']);
+		delete d['$'];
+	}
+	return d;
+}
+
 async function parseNmapOutput(nmapOutput) {
 	const jsonResult = await parseStringPromise(nmapOutput, { explicitArray: false });
 	return formatJSON(jsonResult);
 }
 
-export default { scan, parseNmapOutput };
+export default { scan, parseNmapOutput, bringUp$ };
